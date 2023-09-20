889b45f9b4ed0f9a2f82790ae19a58d1c4057576
M261485 <Mjv@4321>
2023-09-20 13:20:28 -0300

    feat: create loading interceptor

---
+++ src/app/interceptors/loading.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+	HttpRequest,
+	HttpHandler,
+	HttpEvent,
+	HttpInterceptor,
+} from '@angular/common/http';
+import { finalize, Observable } from 'rxjs';
+
+import { LoadingService } from '@services/loading.service';
+
+@Injectable()
+export class HttpLoadingInterceptor implements HttpInterceptor {
+	constructor(private loading: LoadingService) {}
+
+	intercept(
+		request: HttpRequest<any>,
+		next: HttpHandler
+	): Observable<HttpEvent<any>> {
+		this.loading.set(true, request.url);
+
+		return next
+			.handle(request)
+			.pipe(finalize(() => this.loading.set(false, request.url)));
+	}
+}
