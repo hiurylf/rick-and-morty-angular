8bbcefcc2e93197038671bb1c1306cbbf9da4b4f
M261485 <Mjv@4321>
2023-09-20 13:25:23 -0300

    feat: update root routes and added bindToComponentInputs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'home',
+		loadChildren: () =>
+			import('./pages/home/home.module').then(m => m.HomeModule),
+	},
+	{
+		path: 'detail/:id',
+		loadChildren: () =>
+			import('./pages/detail/detail.module').then(m => m.DetailModule),
+	},
+	{ path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [RouterModule.forRoot(routes, { bindToComponentInputs: true })],
 	exports: [RouterModule],
 })
 export class AppRoutingModule {}
